cda2007a671424c9994163984154bd764f1c7d04
Ryan Brazeal @ UFL <45769456+ryan-brazeal-ufl@users.noreply.github.com>
2019-09-15 21:47:22 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author_email='ryan.brazeal@ufl.edu',
     packages=['openpylivox'],
     description='Python3 driver for Livox lidar sensors',
-    packages=find_packages(),    
+    #packages=find_packages(),    
     install_requires=['numpy >= 1.15.4', 'crcmod >= 1.7.0'],
     python_requires='>=3.5',
 )
