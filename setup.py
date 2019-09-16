df29edd3536c106c1f28fca815e2880e0dc39654
Ryan Brazeal @ UFL <45769456+ryan-brazeal-ufl@users.noreply.github.com>
2019-09-15 22:15:20 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='openpylivox',
     version='1.0.0',
-    url='https://github.com/ryan-brazeal-ufl/openpylivox.git',
+    url='https://github.com/ryan-brazeal-ufl/openpylivox',
     author='Ryan Brazeal',
     author_email='ryan.brazeal@ufl.edu',
     packages=['openpylivox'],
-    description='Python3 driver for Livox lidar sensors',
-    #packages=find_packages(),    
+    description='Python3 driver for Livox lidar sensors',    
     install_requires=['numpy >= 1.15.4', 'crcmod >= 1.7.0'],
     python_requires='>=3.5',
 )
