c01a9bbeed59024b4e4c886938847b1cab1f2c95
Ryan Brazeal @ UFL <ryan.brazeal@ufl.edu>
2020-05-29 12:48:57 -0600

    Update setup.py
    included deprecated install requirement

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ setup(
     author_email='ryan.brazeal@ufl.edu',
     packages=['openpylivox'],
     description='Python3 driver for Livox lidar sensors',    
-    install_requires=['numpy >= 1.15.4', 'crcmod >= 1.7.0'],
+    install_requires=['numpy >= 1.15.4', 'crcmod >= 1.7.0', 'deprecated >= 1.2.7'],
     python_requires='>=3.5',
 )
